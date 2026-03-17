d77b79a498a5995bda7afe83c78365fb25ace271
Souradeep Paul <paulsouradeep12@gmail.com>
2026-03-17 20:33:38 +0530

    Initial commit

---
+++ UC1.java
@@ -0,0 +1,10 @@
+public class UC1 {
+
+    /**
+     * Entry point of the application
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
